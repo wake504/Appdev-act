9f47e37e7343b009be7189ca894576b4da89460c
Mark <mdbermudez321@gmail.com>
2025-10-05 18:38:47 +0800

    Console calculator with + and - function

--- ConsoleCalcu/Program.cs
+++ ConsoleCalcu/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program {
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Simple Calculator (Supports '-' and '+')");
+        while (true)
+        {
+
+            Console.Write("Enter first number (input 'q' to quit)\n");
+            var aInput = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(aInput)) continue;
+            if (aInput.Trim().ToLower() == "q") break;
+            if (!double.TryParse(aInput, out double a)) { Console.WriteLine("Invalid number"); continue; }
+
+            Console.Write("Choose operation ('-' or '+')\n");
+            var op = Console.ReadLine()?.Trim();
+
+
+            Console.Write("Enter second Number\n");
+            var bInput = Console.ReadLine();
+            if (!double.TryParse(bInput, out double b)) { Console.WriteLine("Invalid number"); continue; }
+
+            double result = 0;
+            if (op == "+") result = a + b;
+            else if(op == "-") result = a - b;
+            else { Console.WriteLine("Invalid operation"); continue; }
+
+            Console.WriteLine($"Result: {result}\n");
+
+
+        }
+
+    }
+}
